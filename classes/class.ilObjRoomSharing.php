7f3c3e8bde471be9e0824d185273d030856b090c
Fabian Schmid <fs@studer-raimann.ch>
2015-02-24 15:39:30 +0100

    Commit

--- classes/class.ilObjRoomSharing.php
+++ classes/class.ilObjRoomSharing.php
@@ -10,14 +10,17 @@ require_once("Customizing/global/plugins/Services/Repository/RepositoryObject/Ro
  * Mainclass for roomsharing system module. Pool id is the object id
  * and will be stored in the db table "rep_robj_xrs_pools".
  *
- * @author Thomas Matern <tmatern@stud.hs-bremen.de>
- * @author mdazjuk
- * @author troehrig
+ * @author  Thomas Matern <tmatern@stud.hs-bremen.de>
+ * @author  mdazjuk
+ * @author  troehrig
+ *
+ * @author Fabian Schmid <fs@studer-raimann.ch>
+ *
  * @version $Id$
  * @property ilDB $ilDB
  */
-class ilObjRoomSharing extends ilObjectPlugin
-{
+class ilObjRoomSharing extends ilObjectPlugin {
+
 	private $ilDB;
 	private $pool_id;
 	protected $online;
@@ -25,14 +28,15 @@ class ilObjRoomSharing extends ilObjectPlugin
 	/* File id of the rooms agreement */
 	protected $rooms_agreement;
 
+
 	/**
 	 * Constructor of ilObjRoomSharing
 	 *
-	 * @access	public
+	 * @access    public
+	 *
 	 * @param integer $a_ref_id
 	 */
-	public function __construct($a_ref_id = 0)
-	{
+	public function __construct($a_ref_id = 0) {
 		global $ilDB; // needed for db-creation
 		$this->ilDB = $ilDB;
 		parent::__construct($a_ref_id);
@@ -40,58 +44,59 @@ class ilObjRoomSharing extends ilObjectPlugin
 		$this->doRead();
 	}
 
+
 	/**
 	 * Get type.
 	 */
-	public final function initType()
-	{
+	public final function initType() {
 		$this->setType("xrs");
 	}
+
 
 	/**
 	 * Parse properties for sql statements
 	 *
-	 * 	@return array $fields
+	 * @return array $fields
 	 */
-	protected function getDBFields()
-	{
+	protected function getDBFields() {
 		$fields = array(
-			"pool_online" => array("integer", $this->isOnline()),
-			"max_book_time" => array("timestamp", $this->getMaxBookTime()),
-			"rooms_agreement" => array("integer", $this->getRoomsAgreementFileId()));
+			"pool_online" => array( "integer", $this->isOnline() ),
+			"max_book_time" => array( "timestamp", $this->getMaxBookTime() ),
+			"rooms_agreement" => array( "integer", $this->getRoomsAgreementFileId() )
+		);
+
 		return $fields;
 	}
+
 
 	/**
 	 * Create object
 	 *
 	 * @return integer with the pool_id
 	 */
-	protected function doCreate()
-	{
+	protected function doCreate() {
 		parent::doCreate();
 		$this->pool_id = $this->getId();
 		$fields = $this->getDBFields();
-		$fields["id"] = array("integer", $this->pool_id);
-		$fields["max_book_time"] = array("timestamp", "1970-01-01 03:00:00");
-		$fields["rooms_agreement"] = array("integer", "0");
+		$fields["id"] = array( "integer", $this->pool_id );
+		$fields["max_book_time"] = array( "timestamp", "1970-01-01 03:00:00" );
+		$fields["rooms_agreement"] = array( "integer", "0" );
 		$this->ilDB->insert("rep_robj_xrs_pools", $fields);
+
 		return parent::getId();
 	}
+
 
 	/**
 	 * Read data of an RoomSharing-Object from database.
 	 *
 	 */
-	protected function doRead()
-	{
+	protected function doRead() {
 		$objId = $this->getId();
 
-		if ($objId)
-		{
+		if ($objId) {
 			$this->pool_id = $objId;
-			$set = $this->ilDB->query('SELECT * FROM rep_robj_xrs_pools' .
-				' WHERE id = ' . $this->ilDB->quote($this->getId(), 'integer'));
+			$set = $this->ilDB->query('SELECT * FROM rep_robj_xrs_pools' . ' WHERE id = ' . $this->ilDB->quote($this->getId(), 'integer'));
 			$row = $this->ilDB->fetchAssoc($set);
 			$this->setOnline($row['pool_online']);
 			$this->setMaxBookTime($row['max_book_time']);
@@ -99,132 +104,132 @@ class ilObjRoomSharing extends ilObjectPlugin
 		}
 	}
 
+
 	/**
 	 * Update data;
 	 *
 	 * @return bool whether the Update was successful
 	 */
-	protected function doUpdate()
-	{
-		if ($this->getId())
-		{
-			$this->ilDB->update("rep_robj_xrs_pools", $this->getDBFields(),
-				array("id" => array("integer", $this->getId())));
+	protected function doUpdate() {
+		if ($this->getId()) {
+			$this->ilDB->update("rep_robj_xrs_pools", $this->getDBFields(), array( "id" => array( "integer", $this->getId() ) ));
 		}
+
 		return true;
 	}
+
 
 	/**
 	 * Delete data from the database.
 	 *
 	 * @return bool whether the Delete was successful
 	 */
-	protected function doDelete()
-	{
+	protected function doDelete() {
 		ilRoomSharingPoolsHelper::deletePool($this);
+
 		return true;
 	}
+
 
 	/**
 	 * Clones the roomsharing object.
 	 *
 	 * @param ilObjRoomSharing $new_obj The clone.
-	 * @param int $a_target_id
-	 * @param int $a_copy_id
+	 * @param int              $a_target_id
+	 * @param int              $a_copy_id
 	 */
-	protected function doCloneObject($new_obj, $a_target_id, $a_copy_id = null)
-	{
+	protected function doCloneObject($new_obj, $a_target_id, $a_copy_id = NULL) {
 		ilRoomSharingPoolsHelper::clonePool($this, $new_obj);
 	}
+
 
 	/**
 	 * Check status of an room sahring pool.
 	 *
 	 * @param int $a_pool_id
+	 *
 	 * @return boolean true if its online
 	 */
-	public static function _lookupOnline($a_pool_id)
-	{
+	public static function _lookupOnline($a_pool_id) {
 		global $ilDB;
-		$set = $ilDB->query("SELECT pool_online" .
-			" FROM rep_robj_xrs_pools" .
-			" WHERE id = " . $ilDB->quote($a_pool_id, "integer"));
+		$set = $ilDB->query("SELECT pool_online" . " FROM rep_robj_xrs_pools" . " WHERE id = " . $ilDB->quote($a_pool_id, "integer"));
 		$row = $ilDB->fetchAssoc($set);
-		return (bool) $row["pool_online"];
+
+		return (bool)$row["pool_online"];
 	}
+
 
 	/**
 	 * Get online property.
 	 *
 	 * @return bool
 	 */
-	public function isOnline()
-	{
-		return (bool) $this->online;
+	public function isOnline() {
+		return (bool)$this->online;
 	}
+
 
 	/**
 	 * Toggle online property.
 	 *
 	 * @param bool $a_value
 	 */
-	public function setOnline($a_value = true)
-	{
-		$this->online = (bool) $a_value;
+	public function setOnline($a_value = true) {
+		$this->online = (bool)$a_value;
 	}
+
 
 	/**
 	 * Get max booking time.
 	 *
 	 * @return time 'H:i'
 	 */
-	public function getMaxBookTime()
-	{
+	public function getMaxBookTime() {
 		return $this->max_book_time;
 	}
+
 
 	/**
 	 * Set max booking time.
 	 *
 	 * @param datetime $a_max_book_time
 	 */
-	public function setMaxBookTime($a_max_book_time)
-	{
+	public function setMaxBookTime($a_max_book_time) {
 		$this->max_book_time = $a_max_book_time;
 	}
+
 
 	/**
 	 * Returns roomsharing rooms agreement.
 	 *
 	 * @return int file id
 	 */
-	public function getRoomsAgreementFileId()
-	{
+	public function getRoomsAgreementFileId() {
 		return $this->rooms_agreement;
 	}
+
 
 	/**
 	 * Sets roomsharing rooms agreement.
 	 *
 	 * @param integer $a_file_id current file id
 	 */
-	public function setRoomsAgreementFileId($a_file_id)
-	{
+	public function setRoomsAgreementFileId($a_file_id) {
 		$this->rooms_agreement = $a_file_id;
 	}
+
 
 	/**
 	 * Uploads a new rooms agreement by using the ILIAS MediaObject Service.
 	 * If the old file id is given, the old file will be deleted.
 	 *
-	 * @param array $a_newfile an array containing the input values of the form
+	 * @param array  $a_newfile   an array containing the input values of the form
 	 * @param string $a_oldFileId to delete trash
+	 *
 	 * @return string uploaded file id
 	 */
-	public function uploadRoomsAgreement($a_newfile, $a_oldFileId = "0")
-	{
-		if (!empty($a_oldFileId) && $a_oldFileId != "0")
-		{
+	public function uploadRoomsAgreement($a_newfile, $a_oldFileId = "0") {
+		if (!empty($a_oldFileId) && $a_oldFileId != "0") {
 			$agreementFile = new ilObjMediaObject($a_oldFileId);
 			$agreementFile->delete();
 		}
@@ -233,8 +238,7 @@ class ilObjRoomSharing extends ilObjectPlugin
 		$mediaObj->setDescription("RoomSharingRoomsAgreement");
 		$mediaObj->create();
 		$mob_dir = ilObjMediaObject::_getDirectory($mediaObj->getId());
-		if (!is_dir($mob_dir))
-		{
+		if (!is_dir($mob_dir)) {
 			$mediaObj->createDirectory();
 		}
 		$file_name = ilUtil::getASCIIFilename($a_newfile["name"]);
@@ -255,25 +259,24 @@ class ilObjRoomSharing extends ilObjectPlugin
 		return $mediaObj->getId();
 	}
 
+
 	/**
 	 * Set the poolID of bookings
 	 *
 	 * @param integer $pool_id
-	 *        	poolID
+	 *            poolID
 	 */
-	public function setPoolId($pool_id)
-	{
+	public function setPoolId($pool_id) {
 		$this->pool_id = $pool_id;
 	}
+
 
 	/**
 	 * Get the PoolID of bookings
 	 *
 	 * @return integer PoolID
 	 */
-	public function getPoolId()
-	{
-		return (int) $this->pool_id;
+	public function getPoolId() {
+		return (int)$this->pool_id;
 	}
-
 }
